c6ed4f9f8eb54ade957da019711d85133d7418f3
Hein Lin Htet <heinlinhtet3306@gmail.com>
2026-05-29 03:28:00 +0800

    Completed exercises 1.6-1.10

---
+++ unicafe/vite-project/src/App.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react'
+
+const Header = ({header}) => {
+  return (
+    <>
+      <h3>{header}</h3>
+    </>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <>  
+      <button onClick={onClick}>
+        {text}
+      </button>
+    </>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  if (text == "positive") {
+    return (
+      <>
+        <p>
+          {text} {value} %
+        </p>
+      </>
+    )
+  }
+
+  return (
+    <> 
+      <p>
+        {text} {value} 
+      </p> 
+    </>
+  )
+}
+
+const Statistics = (props) => {
+  const [good, neutral, bad, all, average, positive, hasFeedback] = props.stats
+
+  if (!hasFeedback) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positive}/>
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [score, setScore] = useState(0)
+  const [hasFeedback, setHasFeedback] = useState(false)
+
+  const handleClick = text => () => {
+    setHasFeedback(true)
+    let newScore = score
+    let newGood = good
+    if (text == "good") {
+      newScore = score + 1
+      newGood = good + 1
+      setGood(newGood)
+      setScore(newScore)
+    } else if (text == "neutral") {
+      setNeutral(neutral + 1)
+    } else {
+      newScore = score - 1
+      setBad(bad + 1)
+      setScore(newScore)
+    }
+    const newAll = all + 1
+    setAll(newAll)
+    setAverage(newScore / newAll)
+    setPositive((newGood / newAll) * 100)
+  }
+
+  const stats = [
+    good, neutral, bad, all, average, positive, hasFeedback
+  ]
+
+  return (
+    <div>
+      <Header header="Give Feedback:"/>
+      <Button onClick={handleClick("good")} text="good"/>
+      <Button onClick={handleClick("neutral")} text="neutral"/>
+      <Button onClick={handleClick("bad")} text="bad"/>
+      <Header header="Statistics:"/>
+      <Statistics stats={stats}/>
+    </div>
+  )
+}
+
+export default App
